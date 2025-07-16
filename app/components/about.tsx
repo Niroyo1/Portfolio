5d9808fbd557ec48d1a4a2505b76d978dfbe2eb6
Usuario <nicorodriguez9900@gmail.com>
2025-07-16 14:50:57 -0300

    about eslint fix

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -4,6 +4,7 @@ import '../../lib/i18n'
 import { useEffect, useState } from 'react'
 
 export default function About() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { t } = useTranslation()
   const [isClient, setIsClient] = useState(false)
 
